2d805f4b56831d04c9645b5104ed9ec30b297343
cdgn2 <cdgn2@KarurosuAsusPC>
2020-05-24 16:39:57 -0500

    Se corrigio el campo de hora

--- src/Frontera/Agendar.java
+++ src/Frontera/Agendar.java
@@ -20,16 +20,13 @@ public class Agendar extends javax.swing.JPanel {
         jButton1 = new javax.swing.JButton();
         fechaDC = new com.toedter.calendar.JDateChooser();
         jLabel1 = new javax.swing.JLabel();
-        Date date = new Date();
-        SpinnerDateModel sm =
-        new SpinnerDateModel (date, null, null, Calendar.HOUR_OF_DAY);
-        horaS = new javax.swing.JSpinner(sm);
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         sucursalCB = new javax.swing.JComboBox<>();
         motivoCB = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         agendarB = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox<>();
 
         jButton1.setFont(new java.awt.Font("Calibri", 0, 16)); // NOI18N
         jButton1.setText("Ingresar");
@@ -43,10 +40,6 @@ public class Agendar extends javax.swing.JPanel {
 
         jLabel1.setFont(new java.awt.Font("Calibri", 0, 16)); // NOI18N
         jLabel1.setText("Fecha");
-
-        JSpinner.DateEditor de = new JSpinner.DateEditor(horaS, "HH:mm:ss");
-        horaS.setEditor(de);
-        horaS.setFont(new java.awt.Font("Calibri", 0, 16)); // NOI18N
 
         jLabel2.setFont(new java.awt.Font("Calibri", 0, 16)); // NOI18N
         jLabel2.setText("Hora");
@@ -68,6 +61,14 @@ public class Agendar extends javax.swing.JPanel {
         agendarB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 agendarBActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setFont(new java.awt.Font("Calibri", 0, 16)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "09:00 AM", "09:15 AM", "09:30 AM", "09:45 AM", "10:00 AM", "10:15 AM", "10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "01:00 PM", "01:15 PM", "01:30 PM", "01:45 PM", "02:00 PM", "02:15 PM", "02:30 PM", "02:45 PM", "03:00 PM" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
             }
         });
 
@@ -95,9 +96,9 @@ public class Agendar extends javax.swing.JPanel {
                                 .addGap(18, 18, 18)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(sucursalCB, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(horaS)
                             .addComponent(fechaDC, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(motivoCB, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(motivoCB, 0, 214, Short.MAX_VALUE)
+                            .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap(190, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -110,9 +111,9 @@ public class Agendar extends javax.swing.JPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jLabel1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(horaS, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(sucursalCB, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -155,12 +156,16 @@ public class Agendar extends javax.swing.JPanel {
         dialog.setVisible(true);
     }//GEN-LAST:event_agendarBActionPerformed
 
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton agendarB;
     private com.toedter.calendar.JDateChooser fechaDC;
-    private javax.swing.JSpinner horaS;
     private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
